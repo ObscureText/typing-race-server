3768cb8ecd49128c354f3920b4e4bd3d238a729d
favastechfriar <favasmonu555@gmail.com>
2024-11-09 00:49:56 +0530

    basic working model

--- socket/listeners/listeners.js
+++ socket/listeners/listeners.js
@@ -1,12 +1,9 @@
 const events = require("../events/events");
 
 const configureSocketListeners = (io) => {
-    let onlineUsersCount = 0;
-    let rooms = [];
+    let players = [];
 
     io.on(events.listen.CONNECTION, (socket) => {
-        onlineUsersCount++;
-        io.emit(events.emit.ONLINE_USERS_COUNT, onlineUsersCount);
         console.log("New user connected", socket.id);
 
         /*-------------------------------------------------------------------------------------------*/
@@ -17,55 +14,39 @@ const configureSocketListeners = (io) => {
         /*-------------------------------------------------------------------------------------------*/
         /*-------------------------------------------------------------------------------------------*/
 
-        socket.on(events.listen.CREATE_ROOM, ({ roomId, roomName, isPrivate }) => {
-            const room = {
-                roomId: roomId,
-                roomName: roomName,
-                isPrivate: isPrivate,
-                players: [],
-            };
-            rooms = [...rooms, room];
-            console.log("Socket rooms - ", rooms);
+        socket.on(events.listen.FIND_OPPONENT, ({ socketId, nickName }) => {
+            if (socketId) {
+                if (players.length === 0) {
+                    players = [{ socketId, nickName }];
+                } else {
+                    const existingPlayer = players[0];
+                    const arrivedPlayer = { socketId, nickName };
+                    
+                    console.log('Arriving', arrivedPlayer);
+                    console.log('Existing', existingPlayer);
+                    
+                    players = players.filter((_, index) => index !== 0);
+                    socket.emit(events.emit.OPPONENT_FOUND, existingPlayer);
+                    io.to(existingPlayer.socketId).emit(
+                        events.emit.OPPONENT_FOUND,
+                        arrivedPlayer,
+                    );
+                }
+            }
         });
 
-        socket.on(events.listen.JOIN_ROOM, ({ nickName, isOwner, socketId, roomId }) => {
-            const player = { nickName, isOwner, socketId };
-            rooms = rooms.map((room) => {
-                if (room.roomId === roomId) {
-                    const players = room.players || [];
-                    return {
-                        ...room,
-                        players: [...players, player]
-                    }
-                } else {
-                    return room;
-                }
-            });
-            const room = rooms.find((room) => room.roomId === roomId);
-            const players = room?.players || [];
-            players.forEach((plr) => {
-                io.to(plr.socketId).emit(events.emit.PLAYER_JOINED, players);
-            })
+        socket.on(events.listen.STOP_WAITING, (socketId) => {
+            players = players.filter((player) => player.socketId !== socketId);
+            console.log('Waiting stop', players)
         });
 
+        socket.on(events.listen.USER_DISCONNECTED, (opponentSocketId) => {
+            io.to(opponentSocketId).emit(events.emit.OPPONENT_DISCONNECTED);
+        });
 
-        socket.on(events.listen.EXIT_ROOM, ({ socketId, roomId }) => {
-            rooms = rooms.map((room) => {
-                if (room.roomId === roomId) {
-                    const players = room.players.filter((player) => player.socketId !== socketId);
-                    return {
-                        ...room,
-                        players
-                    }
-                } else {
-                    return room;
-                }
-            });
-            const room = rooms.find((room) => room.roomId === roomId);
-            const players = room?.players || [];
-            players.forEach((plr) => {
-                io.to(plr.socketId).emit(events.emit.PLAYER_LEAVED, players);
-            })
+        socket.on(events.listen.UPDATE_SCORE, ({ opponentSocketId, score }) => {
+            socket.emit(events.emit.SCORE_UPDATED, { currentScore: score });
+            io.to(opponentSocketId).emit(events.emit.SCORE_UPDATED, { opponentScore: score });
         });
 
         /*-------------------------------------------------------------------------------------------*/
@@ -76,8 +57,6 @@ const configureSocketListeners = (io) => {
         /*-------------------------------------------------------------------------------------------*/
 
         socket.on(events.listen.DISCONNECT, () => {
-            onlineUsersCount--;
-            io.emit(events.emit.ONLINE_USERS_COUNT, onlineUsersCount);
             console.log("User disconnected", socket.id);
         });
     });
